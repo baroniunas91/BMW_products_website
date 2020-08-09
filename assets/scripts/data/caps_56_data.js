43f2e058017be76d85691f9c6d9994bde517a6cc
Edgaras <baroniunas@gmail.com>
2020-08-09 11:44:24 +0300

    Update

--- assets/scripts/data/caps_56_data.js
+++ assets/scripts/data/caps_56_data.js
@@ -4,6 +4,7 @@ export default [
         path: './assets/images/BW56.jpg',
         code: 'Kodas: BW56'
     },
+    //Neatyko
     // {
     //     gallery: 'gallery3',
     //     path: './assets/images/BB68.jpg',
