27323d8f801ea1e47a27c63129a5aec3bc7fa0af
Edgaras <baroniunas@gmail.com>
2020-08-09 11:26:11 +0300

    Change photos

--- assets/scripts/data/caps_56_data.js
+++ assets/scripts/data/caps_56_data.js
@@ -4,16 +4,16 @@ export default [
         path: './assets/images/BW56.jpg',
         code: 'Kodas: BW56'
     },
-    {
-        gallery: 'gallery3',
-        path: './assets/images/BB68.jpg',
-        code: 'Kodas: BB56'
-    },
-    {
-        gallery: 'gallery3',
-        path: './assets/images/FB68.jpg',
-        code: 'Kodas: FB56'
-    },
+    // {
+    //     gallery: 'gallery3',
+    //     path: './assets/images/BB68.jpg',
+    //     code: 'Kodas: BB56'
+    // },
+    // {
+    //     gallery: 'gallery3',
+    //     path: './assets/images/FB68.jpg',
+    //     code: 'Kodas: FB56'
+    // },
     {
         gallery: 'gallery3',
         path: './assets/images/56.jpg',
