9707bd41f234a57b96258559e541526c4993a367
baroniunas91 <61449435+baroniunas91@users.noreply.github.com>
2021-03-30 18:15:58 +0300

    update

--- assets/scripts/data/caps_60_data.js
+++ assets/scripts/data/caps_60_data.js
@@ -4,16 +4,16 @@ export default [
         path: './assets/images/BW60.jpg',
         code: 'Kodas: BW60'
     },
-    {
-        gallery: 'gallery2',
-        path: './assets/images/BB60.jpg',
-        code: 'Kodas: BB60'
-    },
-    {
-        gallery: 'gallery2',
-        path: './assets/images/FB60.jpg',
-        code: 'Kodas: FB60'
-    },
+    // {
+    //     gallery: 'gallery2',
+    //     path: './assets/images/BB60.jpg',
+    //     code: 'Kodas: BB60'
+    // },
+    // {
+    //     gallery: 'gallery2',
+    //     path: './assets/images/FB60.jpg',
+    //     code: 'Kodas: FB60'
+    // },
     {
         gallery: 'gallery2',
         path: './assets/images/60.jpg',
--- assets/scripts/data/caps_68_data.js
+++ assets/scripts/data/caps_68_data.js
@@ -4,21 +4,21 @@ export default [
         path: './assets/images/BW68.jpg',
         code: 'Kodas: BW68'
     },
-    {
-        gallery: 'gallery1',
-        path: './assets/images/BB68.jpg',
-        code: 'Kodas: BB68'
-    },
-    {
-        gallery: 'gallery1',
-        path: './assets/images/FB68.jpg',
-        code: 'Kodas: FB68'
-    },
-    {
-        gallery: 'gallery1',
-        path: './assets/images/M68.jpg',
-        code: 'Kodas: M68'
-    },
+    // {
+    //     gallery: 'gallery1',
+    //     path: './assets/images/BB68.jpg',
+    //     code: 'Kodas: BB68'
+    // },
+    // {
+    //     gallery: 'gallery1',
+    //     path: './assets/images/FB68.jpg',
+    //     code: 'Kodas: FB68'
+    // },
+    // {
+    //     gallery: 'gallery1',
+    //     path: './assets/images/M68.jpg',
+    //     code: 'Kodas: M68'
+    // },
     {
         gallery: 'gallery1',
         path: './assets/images/AP68.jpg',
